41f7ce6d3c73144742b21343e285b5f7f8d43c33
Benjamin <eunvanz@naver.com>
2022-02-20 09:34:21 +0900

    style: add pulse animation to text

--- src/components/GameOverNext/GameOverNext.tsx
+++ src/components/GameOverNext/GameOverNext.tsx
@@ -8,7 +8,7 @@ export interface GameOverNextProps {
 const GameOverNext: React.FC<GameOverNextProps> = ({ onNext }) => {
   return (
     <div css={tw`flex flex-col items-center`}>
-      <h1>Click Pokémons to check the answers</h1>
+      <h1 css={tw`animate-pulse`}>Click Pokémons to check the answers</h1>
       <h3 css={tw`text-gray-500 mb-2`}>or</h3>
       <Button css={tw`w-full`} onClick={onNext}>
         Check your rank
