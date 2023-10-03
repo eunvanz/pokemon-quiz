f5c53ec58eea13e15d62b4380bce0d7a5d541507
Benjamin <eunvanz@naver.com>
2023-10-03 15:55:26 +0900

    feat: apply responsive to leaderboard

--- components/rank-table/rank-table.tsx
+++ components/rank-table/rank-table.tsx
@@ -52,11 +52,68 @@ const RankTable: React.FC<RankTableProps> = ({
 
   const Row = useCallback(
     ({ item, className }: { item: RankItem; className?: string }) => {
-      return (
+      return isMobile ? (
+        <div
+          className={classNames(
+            'flex w-full gap-2 flex-row items-start text-sm py-4 border-b border-gray-300',
+            className,
+          )}
+        >
+          <div className="text-primary w-14">{item.seq.toLocaleString()}</div>
+          <div className="flex flex-col gap-2 w-full">
+            <div>{item.name}</div>
+            <div className="flex flex-row justify-between">
+              <div className="text-secondary">Score</div>
+              <div className="text-primary" css={{ zIndex: -1 }}>
+                <AnimatedNumber value={item.score} hasComma size={16} />
+              </div>
+            </div>
+            <div className="flex flex-row gap-8">
+              <div className="flex flex-row justify-between w-full">
+                <div className="text-secondary">Generation</div>
+                <div>{item.generation === 0 ? 'All' : item.generation}</div>
+              </div>
+              <div className="flex flex-row justify-between w-full">
+                <div className="text-secondary">Gotcha</div>
+                <div>{item.gotcha.toLocaleString()}</div>
+              </div>
+            </div>
+            <div className="flex flex-row gap-8">
+              <div className="flex flex-row justify-between w-full">
+                <div className="text-secondary">Max Combo</div>
+                <div>{item.maxCombo.toLocaleString()}</div>
+              </div>
+              <div className="flex flex-row justify-between w-full">
+                <div className="text-secondary">Accuracy</div>
+                <div>
+                  {item.accuracy}
+                  <span className="text-gray-400 text-xs">%</span>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-row gap-8">
+              <div className="flex flex-row justify-between w-full">
+                <div className="text-secondary">Avg Spd</div>
+                <div>
+                  {item.avgSpeed.toLocaleString()}
+                  <span className="text-gray-400 text-xs">wpm</span>
+                </div>
+              </div>
+              <div className="flex flex-row justify-between w-full">
+                <div className="text-secondary">Max Spd</div>
+                <div>
+                  {item.maxSpeed.toLocaleString()}
+                  <span className="text-gray-400 text-xs">wpm</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
         <div
           role="row"
           className={classNames(
-            'flex w-full flex-row p-2 h-20 gap-2 items-center',
+            'flex flex-row p-2 h-20 gap-2 items-center',
             className,
           )}
         >
@@ -108,41 +165,43 @@ const RankTable: React.FC<RankTableProps> = ({
         </div>
       )
     },
-    [ROW_CLASSNAMES],
+    [ROW_CLASSNAMES, isMobile],
   )
 
   return (
-    <div role="table" className="max-w-6xl mx-4" css={{ minWidth: '960px' }}>
-      <div className="flex flex-col bg-white top-0 sticky gap-8 pt-4">
+    <div role="table" className="w-full max-w-6xl mx-4">
+      <div className="flex flex-col bg-white top-0 sticky gap-8 pt-4 pb-4 sm:pb-0">
         <h1 className="text-2xl sm:text-4xl">
           <Link href="/" className="text-primary hover:text-blue-600">
             Pokédrops
           </Link>{' '}
           Leaderboard
         </h1>
-        <div
-          role="row"
-          className="flex w-full flex-row p-2 border-b border-gray-400 text-secondary text-xs gap-2"
-        >
-          {ROW_CLASSNAMES.map((className, idx) => {
-            const labels = [
-              'RANK',
-              'NAME',
-              'SCORE',
-              'GENERATION',
-              'GOTCHA',
-              'MAX COMBO',
-              'AVG SPEED',
-              'MAX SPEED',
-              'ACCURACY',
-            ]
-            return (
-              <div key={idx} className={`flex ${className}`}>
-                {labels[idx]}
-              </div>
-            )
-          })}
-        </div>
+        {!isMobile && (
+          <div
+            role="row"
+            className="flex w-full flex-row p-2 border-b border-gray-400 text-secondary text-xs gap-2"
+          >
+            {ROW_CLASSNAMES.map((className, idx) => {
+              const labels = [
+                'RANK',
+                'NAME',
+                'SCORE',
+                'GENERATION',
+                'GOTCHA',
+                'MAX COMBO',
+                'AVG SPEED',
+                'MAX SPEED',
+                'ACCURACY',
+              ]
+              return (
+                <div key={idx} className={`flex ${className}`}>
+                  {labels[idx]}
+                </div>
+              )
+            })}
+          </div>
+        )}
       </div>
       {items?.map((item) => <Row key={item.id} item={item} />)}
       {hasNextPage && !isLoadingNextPage && (
