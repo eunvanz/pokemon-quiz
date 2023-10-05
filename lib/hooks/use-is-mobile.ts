310711ac9a658332280fea22b7d471a598163b3e
Benjamin <eunvanz@naver.com>
2023-10-05 21:44:03 +0900

    feat: develop useIsMobile hook

--- lib/hooks/use-is-mobile.ts
+++ lib/hooks/use-is-mobile.ts
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import { useIsomorphicLayoutEffect, useWindowSize } from 'usehooks-ts'
+import { checkIsSSR } from '../helpers/common'
 
 const useIsMobile = () => {
   const { width } = useWindowSize()
 
-  const [isMobile, setIsMobile] = useState(width < 640)
+  const [isMobile, setIsMobile] = useState(
+    checkIsSSR() ? false : window.innerWidth < 640,
+  )
 
   useIsomorphicLayoutEffect(() => {
     setIsMobile(width < 640)
